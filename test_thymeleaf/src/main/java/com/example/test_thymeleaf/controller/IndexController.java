e7803664ee3d6ea2be5114a7ee7490b7cab2c401
Estefani <estefania@outlook.com>
2019-02-27 18:08:58 -0600

    create a spring mvc project with thymeleaf

---
+++ test_thymeleaf/src/main/java/com/example/test_thymeleaf/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.test_thymeleaf.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@Value(value = "${application.controller.title}")
+	private String title;
+	
+	@GetMapping("/greet")
+	public String getIndex(Model model)
+	{
+		model.addAttribute("title",this.title);
+		return "greet"; 
+	}
+}
