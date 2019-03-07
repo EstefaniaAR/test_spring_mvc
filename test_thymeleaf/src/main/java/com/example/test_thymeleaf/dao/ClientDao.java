3fc6d3ecc1e05003d31bc1a9e221d1d0f5c4f76a
Estefani <estefania@outlook.com>
2019-03-07 17:36:21 -0600

    ClientRepository was deleted. This is a better for to implement a CRUD interface because you don't need to worry about the method implementations or queries

--- test_thymeleaf/src/main/java/com/example/test_thymeleaf/dao/ClientDao.java
+++ test_thymeleaf/src/main/java/com/example/test_thymeleaf/dao/ClientDao.java
@@ -1,11 +1,12 @@
 package com.example.test_thymeleaf.dao;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.test_thymeleaf.domain.Client;
 
+@Repository
 public interface ClientDao extends CrudRepository<Client,Long>
 {
 
